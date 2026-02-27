f8c4f3760315bf02aedc25dbf4e648827e369832
Camila Domato <camiladomato@gmail.com>
2026-02-26 21:38:47 -0300

    ADD estilos en componentes

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,13 +1,37 @@
-
+import './styles.css'
 
 const Contacts = () => {
   return (
-    <section>
-        <li>contactos</li>
-        <li>contactos</li>
-        <li>contactos</li>
-        <li>contactos</li>
-        <li>contactos</li>
+    <section className='chats'>
+        <ul>
+          <li className='user-chat'>
+            <img src="./src/assets/dami.jpg" alt="foto de perfil de ..." />
+             <div class="chat-info">
+              <h3>Damian Benitez</h3>
+              <p>ultimo msj</p>
+            </div>
+           <span class="time">11.20</span>
+          </li>
+
+          <li className='user-chat'>
+            <img src="./src/assets/muichiro.jpg" alt="foto de perfil de ..." />
+             <div class="chat-info">
+              <h3>Grupo UTN</h3>
+              <p>ultimo msj</p>
+            </div>
+           <span class="time">11.20</span>
+          </li>
+
+          <li className='user-chat'>
+            <img src="./src/assets/mv.png" alt="foto de perfil de ..." />
+             <div class="chat-info">
+              <h3>Movistar</h3>
+              <p>ultimo msj</p>
+            </div>
+           <span class="time">11.20</span>
+          </li>
+          
+        </ul>
     </section>
   )
 }
--- src/components/NavChat/Navchat.jsx
+++ src/components/NavChat/Navchat.jsx
@@ -1,8 +1,18 @@
-
+import './styles.css'
 
 const NavChat = () => {
   return (
-    <div>NavChat</div>
+    <div className="chat">
+      <header class="chat-header">
+        <img
+          src="https://media.licdn.com/dms/image/v2/D4E03AQFrq2Y1y0EytQ/profile-displayphoto-crop_800_800/B4EZo370E3KoAI-/0/1761875018739?e=1770854400&v=beta&t=-VqvJ35aPpuQCM4uPJEGZwzMARBk3R-ufbJdRCGW3i0"
+          alt="foto de perfil" />
+        <div>
+          <h3>Damian Benitez</h3>
+          <p>últ. vez hoy a la(s) 15:47 p. m.</p>
+        </div>
+      </header>
+    </div>
   )
 }
 
