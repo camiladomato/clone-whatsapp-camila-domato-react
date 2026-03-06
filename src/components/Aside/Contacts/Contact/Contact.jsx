75692a5d0ccc67ac36999d6fff152012bf417e2c
Camila Domato <camiladomato@gmail.com>
2026-03-06 09:00:39 -0300

    ADD destructuring y msj dinamico para ver ultimo msj en contacto

--- src/components/Aside/Contacts/Contact/Contact.jsx
+++ src/components/Aside/Contacts/Contact/Contact.jsx
@@ -1,14 +1,13 @@
 
 
-const Contact = ({img , nombre , msj , time}) => {
- 
+const Contact = ({img , nombre , mensajes , time}) => {
   return (
     <>
         <li className='user-chat'>
             <img src={img} alt="foto de perfil de ..." />
              <div className="chat-info">
               <h3>{nombre}</h3>
-              <p>{msj}</p>
+              <p>{mensajes[mensajes.length -1].text}</p>
             </div>
            <span className="time">{time}</span>
           </li>
--- src/components/Aside/Contacts/Contacts.jsx
+++ src/components/Aside/Contacts/Contacts.jsx
@@ -8,7 +8,7 @@ const Contacts = ({contactos}) => {
     <section className='chats'>
         <ul>
               {contactos.map((contacto) => (
-                <Contact img={contacto.img} nombre={contacto.nombre} msj={contacto.msj} time={contacto.time} key={contacto.id}>
+                <Contact img={contacto.img} nombre={contacto.nombre} mensajes={contacto.mensajes} time={contacto.time} key={contacto.id}>
                 </Contact>
       ))}
         </ul>
