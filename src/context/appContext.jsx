aabb2d8000c44fc71029ce9f4a440b6ba4ed6a26
Camila Domato <camiladomato@gmail.com>
2026-03-09 15:19:37 -0300

    Add logout

--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -2,13 +2,15 @@ import { useContext, createContext, useState } from "react";
 import { users } from "../services/mockApi.js"
 
 
+
 const appContext = createContext()
 
 const AppProvider = ({ children }) => {
-
-    const [user, setUser] = useState(null)
+    const localUser = JSON.parse(localStorage.getItem("user"))
+    const [user, setUser] = useState(localUser || null)
     const [contactSelect, setContactSelect] = useState({})
 
+   
 
     const login = (emailUser, passwordUser) => {
         const userLogueado = users.find((user) => user.email === emailUser && user.password === passwordUser)
@@ -17,6 +19,7 @@ const AppProvider = ({ children }) => {
             return false
         } else {
             setUser(userLogueado)
+            localStorage.setItem("user",JSON.stringify(userLogueado))
             return true
         }
 
@@ -27,9 +30,12 @@ const AppProvider = ({ children }) => {
         setContactSelect(user.contactos.find((contacto) => contacto.id == idUser))
     }
 
+    const logOut = () =>{
+        localStorage.removeItem("user")
+    }
 
     return (
-        <appContext.Provider value={{user, contactSelect, setContactSelect, idContactoSeleccionado, login }}>
+        <appContext.Provider value={{user, contactSelect, setContactSelect, idContactoSeleccionado, login , logOut}}>
             {children}
         </appContext.Provider>
     )
